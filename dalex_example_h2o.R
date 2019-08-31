1eb7ff838dd07f57c0ff6a067562723e7b21b48e
Bruno Testaguzza Carlin <brunotestaguzza@gmail.com>
2019-08-31 00:43:28 -0300

    It works fine

--- dalex_example_h2o.R
+++ dalex_example_h2o.R
@@ -15,25 +15,41 @@ model <- h2o::h2o.gbm(
   min_rows =  12,
   learn_rate = 0.001
 )
-
 explainer <- explain_h2o(model, titanic_test[,1:17], titanic_test[,18])
+
+x_dalex <- simple_test %>% select(-response)
+y_dalex <- simple_test %>%
+  transmute(response = response %>%
+              as.numeric()) %>% 
+  mutate(response = if_else(response == 1,
+                            0,
+                            1)) %>% as.data.frame()
+y_dalex <- y_dalex[,1]
+  
+  
+explainer <- explain_h2o(aml@leader,x_dalex,y_dalex)
+
+
 mp_h2o_gbm <- model_performance(explainer)
 
 plot(mp_h2o_gbm)
 plot(mp_h2o_gbm, geom = "boxplot")
 
-mpp_h2o_glm <- accumulated_dependency(explainer,variables = "fare")
+
+mpp_h2o_glm <- accumulated_dependency(explainer,variables = "B1_2")
 mpp_h2o_glm
 plot(mpp_h2o_glm)
+
+
 
 library(ingredients)
 plot(feature_importance(explainer))
 describe(feature_importance(explainer))
 
 library(iBreakDown)
-plot(break_down(explainer, titanic_test[2,1:17]))
+plot(break_down(explainer, x_dalex[1,]))
 
-describe(break_down(explainer, titanic_test[2,1:17]))
+describe(break_down(explainer,x_dalex[1,]))
 
 
 library(shapper)
